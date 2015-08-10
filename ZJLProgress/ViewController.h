2bf554c0f9c0e43491e15bfeebce14b10afab791
钟进林 <zhongjinlin@zhongjinlindeMacBook-Pro.local>
2015-08-10 19:29:59 +0800

    Initial Commit

---
+++ ZJLProgress/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZJLProgress
+//
+//  Created by 钟进林 on 15/8/10.
+//  Copyright (c) 2015年 zixiadaxian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ZJLProgress/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ZJLProgress
+//
+//  Created by 钟进林 on 15/8/10.
+//  Copyright (c) 2015年 zixiadaxian. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
